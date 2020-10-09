759742f0832f2ace33fc0cff9ca6b7abfecdc63c
Alexander Björling <alexander.bjorling@maxiv.lu.se>
2020-10-09 07:47:10 +0200

    A dummy receiver that plots spectra

--- DummyReceiver.py
+++ DummyReceiver.py
@@ -1,14 +1,18 @@
 import zmq
 import numpy as np
+import matplotlib.pyplot as plt
+plt.ion()
 
 class Receiver(object):
-    def __init__(self, port=9999):
+    def __init__(self, port=9999, host='localhost'):
         context = zmq.Context()
         self.sock = context.socket(zmq.SUB)
-        self.sock.connect ("tcp://localhost:%u" % port)
+        self.sock.connect ("tcp://%s:%u" % (host, port))
         self.sock.setsockopt(zmq.SUBSCRIBE, b"")
 
     def run(self):
+        plt.figure()
+        plt.gcf().suptitle('channel 0')
         while True:
             meta = self.sock.recv_json()
             print(meta)
@@ -16,8 +20,12 @@ class Receiver(object):
                 buff = self.sock.recv()
                 m, n = meta['shape'][:2]
                 frame = np.frombuffer(buff, dtype=meta['type']).reshape((m, n))
+                plt.gca().clear()
+                plt.plot(frame[0,:])
+                plt.pause(.01)
                 print(frame.shape, frame.dtype)
 
 if __name__ == '__main__':
-    r = Receiver()
+    r = Receiver(host='b303a-a100380-cab01-dia-detxfcu-01')
     r.run()
+
