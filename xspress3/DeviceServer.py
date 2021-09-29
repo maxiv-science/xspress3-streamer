1d47d8c0abd95c5bb6b4319f76555d9244ce38d0
Paul Bell <paubel@w-v-sedslab-cc-0.maxiv.lu.se>
2021-09-29 09:07:28 +0200

    memorise file name for Balder

--- xspress3/DeviceServer.py
+++ xspress3/DeviceServer.py
@@ -73,7 +73,7 @@ class StandardDetector(object):
         assert (val in valid), 'TriggerMode can be %s'%(' or '.join(valid))
         self._triggermode = val
 
-    @attribute(dtype=str)
+    @attribute(dtype=str, hw_memorized=True)
     def DestinationFileName(self):
         return self._destinationfilename
 
